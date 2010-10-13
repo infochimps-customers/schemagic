dc4d72625070f112dfb044b3bee6608055ae2e4d
David <dsnyder90@gmail.com>
2010-10-13 14:51:03 -0500

    can add and remove fields with jquery

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def link_to_add_fields(name,form)
+    fields = form.fields_for(:field, Field.new, :child_index => "new_field") do |builder|
+      render "field", :f => builder
+    end
+    link_to_function(name,"add_block(this,\'#{escape_javascript(fields)}\')")
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,14 @@
 function remove_block(link) {
-$(link).closest(".flowbox").hide();    
+  $(link).closest(".field_flowbox").fadeOut('slow', function(){});
 }
+
+function add_block(link,content) {
+  $(link).closest("form").append(content);
+}
+
+function swap_block_up(link) {
+  $(link).closest(".field_flowbox").append($(link).previous(".flowbox"));
+}
+
+
+
