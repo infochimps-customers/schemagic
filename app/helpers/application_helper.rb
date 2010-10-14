97437f93275ffb5c1fc03b2fb7742d99d9ec6971
David <dsnyder90@gmail.com>
2010-10-14 17:59:09 -0500

    access fields attribute correctly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
   def link_to_add_fields(name,form)
-    fields = form.fields_for(:field, Field.new) do |builder|
-      render "field", :f => builder
-    end
+    fields = form.fields_for :fields,Field.new do |builder|
+               render "field", :f => builder
+             end
     link_to_function(name,"add_block(this,\'#{escape_javascript(fields)}\')")
   end
+
 end
