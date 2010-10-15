fc2aa7e1e92c4b953ace6debcd0dded1ebabe00a
David Snyder <dsnyder90@gmail.com>
2010-10-15 03:53:23 -0500

    keeping the flow on one page

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-   map.root :controller => "datasets"
+   map.root :controller => "datasets", :action => "new"
 
   # See how all your routes lay out with "rake routes"
 
