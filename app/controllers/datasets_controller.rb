4439c4b02b49e9de7afe14a76cfe24cf12e646f0
David <dsnyder90@gmail.com>
2010-10-18 12:18:48 -0500

    switched to UUID to obscure dataset paths

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -11,24 +11,24 @@ class DatasetsController < ApplicationController
 
   def create
     @dataset = Dataset.new(params[:dataset])
+    @dataset.generate_uuid!
     if @dataset.save
-      redirect_to @dataset
+      redirect_to dataset_path(@dataset.uuid)
     else
       render :action => "new"
     end
   end
 
   def show
-    @dataset = Dataset.find(params[:id])
-
+    @dataset = (Dataset.find_by_uuid(params[:uuid]) or raise ActiveRecord::RecordNotFound.new)
   end
 
   def edit
-    @dataset = Dataset.find(params[:id])
+    @dataset = (Dataset.find_by_uuid(params[:uuid])or raise ActiveRecord::RecordNotFound.new)
   end
 
   def update
-    @dataset = Dataset.find(params[:id])
+    @dataset = Dataset.find_by_uuid(params[:uuid])
     if @dataset.update_attributes(params[:dataset])
       flash[:notice] = "Updated Dataset Successfully"
       redirect_to @dataset
--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -1,10 +1,17 @@
 class Dataset < ActiveRecord::Base
 
+  require 'uuidtools'
+
   attr_accessible :data_files_attributes,:contact_name, :title, :company_name, :phone, :description, :email
 
   has_many :data_files, :dependent => :destroy
   accepts_nested_attributes_for :data_files, :reject_if => lambda { |attrs| attrs['title'].blank?}, :allow_destroy => true
 
   validates_presence_of :title
+  validates_presence_of :uuid
+
+  def generate_uuid!
+    self.uuid = "%032x" % UUIDTools::UUID.timestamp_create.to_i
+  end
 
 end
