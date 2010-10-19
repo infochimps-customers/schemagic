893b24cf586a345f5a9294145df04fd61e2ca974
David <dsnyder90@gmail.com>
2010-10-19 18:56:51 -0500

    field data_type select options werent using their set value on edit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,16 @@
 module ApplicationHelper
 
   def gen_field_template(form)
-    form.fields_for :fields,Field.new, :child_index => "new_field" do |builder|
-      render "field", :f => builder
+    @new_field = Field.new
+    form.fields_for :fields,@new_field, :child_index => "new_field" do |builder|
+      render :partial => "field", :locals => { :f => builder, :field => @new_field}
     end
   end
 
   def gen_data_file_template(form)
-    form.fields_for :data_files,DataFile.new, :child_index => "new_data_file" do |builder|
-      render "data_file", :f => builder
+    @new_data_file = DataFile.new
+    form.fields_for :data_files,@new_data_file, :child_index => "new_data_file" do |builder|
+      render :partial => "data_file", :locals => { :f => builder, :data_file => @new_data_file}
     end
   end
 
