8612fe4d43ab38c70d7155636b9cf8e9fd6a9929
David <dsnyder90@gmail.com>
2010-10-22 14:03:04 -0500

    send emails to the right place

--- app/models/emailer.rb
+++ app/models/emailer.rb
@@ -1,7 +1,7 @@
 class Emailer < ActionMailer::Base
 
   def schema(dataset)
-    recipients "david@infochimps.org"
+    recipients env_email
     from       "no-reply@infochimps.org"
     subject    "#{dataset.company_name} submitted a schema on Schemagic!"
     sent_on    Time.now
@@ -11,5 +11,9 @@ class Emailer < ActionMailer::Base
     #   a.body = ActionController::Base.render_to_string :file => "/app/views/emailer/schema.erb", :dataset => dataset
     # end
 
+  end
+
+  def env_email
+    RAILS_ENV == "production" ? "bd@infochimps.org" : "infochimps@mailinator.com"
   end
 end
