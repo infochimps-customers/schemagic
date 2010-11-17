803f7588fe5725ef4022a9fa049fc2a64c1658c1
David Snyder <david@infochimps.org>
2010-11-17 13:35:27 -0600

    dont need to build avro hash in data_file model

--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -8,18 +8,4 @@ class DataFile < ActiveRecord::Base
 
   validates_presence_of :title
 
-  def build_record
-    avro_record = { }
-    avro_record[:name] = self.title
-    avro_record[:type] = "record"
-    avro_record[:fields] = []
-    self.fields.by_column.each do |field|
-      attrs = field.attributes
-      #'type' is a reserved type in ruby for STI, so we'll just switch it over here
-      attrs["type"] = attrs["data_type"]
-      avro_record[:fields] << attrs.to_json(:only => ["name", "type","doc"])
-    end
-    avro_record
-  end
-
 end
